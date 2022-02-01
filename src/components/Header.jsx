2196fd5c07c5a1325059ab372746de78258cc8a0
berninup <chris-basham@live.com>
2022-02-01 10:05:11 -0600

    links added

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+import NewButton from './NewButton'
+
+function Header() {
+  return (
+  <div>
+      
+    <NewButton />  
+  </div>
+  )
+}
+
+export default Header;
