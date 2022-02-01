b503813074659189c149608ba920e0ed633303d5
Abraham Li <liabraham.1999@gmail.com>
2022-02-01 13:48:58 -0700

    added delete and show

--- src/pages/Note.jsx
+++ src/pages/Note.jsx
@@ -1,4 +1,12 @@
-import { useState, useEffect } from 'react'
-
-function Show() {}
+function Show(props) {
+	const id = props.match.params.id
+	const notes = props.notes
+	const note = notes.find((p) => p._id === id)
+	return (
+		<div>
+			<h1>{note.title}</h1>
+			<h3>{note.content}</h3>
+		</div>
+	)
+}
 export default Show
